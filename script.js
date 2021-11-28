58c77f1321f7b6c54c2ef575dbacdc6cbeeadfdd
Ivan Milenkovic <argentinaivan@hotmail.com>
2021-11-28 23:46:03 +0100

    update

--- script.js
+++ script.js
@@ -1,40 +1,83 @@
-const aaa = "https://openexchangerates.org/api/currencies.json";
-const url = "https://api.exchangerate.host/latest";
+const currencies = "https://openexchangerates.org/api/currencies.json";
+
+const input = document.querySelector("#amount");
+const button = document.querySelector("#convert");
+const swap = document.querySelector(".iconBox");
+const resultHeading = document.querySelector("#result");
+const fromToText = document.querySelector("#fromTo");
+const toFromText = document.querySelector("#toFrom");
 
 const select = document.querySelectorAll("select");
 
 async function getCurrencies() {
-  const response = await fetch(aaa);
+  const response = await fetch(currencies);
   const data = await response.json();
 
-  console.log(data);
-  const entries = Object.keys(data);
+  const keys = Object.keys(data);
   const values = Object.values(data);
-  console.log(entries, values);
 
-  for (let i = 0; i < entries.length; i++) {
-    if (entries[i] == "CLF") continue;
-    if (entries[i] == "CNH") continue;
-    if (entries[i] == "MRO") continue;
-    if (entries[i] == "STD") continue;
-    if (entries[i] == "VEF") continue;
-    if (entries[i] == "XAG") continue;
-    if (entries[i] == "XAU") continue;
-    if (entries[i] == "XPD") continue;
-    if (entries[i] == "XPT") continue;
+  for (let i = 0; i < keys.length; i++) {
+    if (keys[i] == "CLF") continue;
+    if (keys[i] == "CNH") continue;
+    if (keys[i] == "MRO") continue;
+    if (keys[i] == "STD") continue;
+    if (keys[i] == "VEF") continue;
+    if (keys[i] == "XAG") continue;
+    if (keys[i] == "XAU") continue;
+    if (keys[i] == "XPD") continue;
+    if (keys[i] == "XPT") continue;
 
-    select[0].innerHTML += `<option value="${entries[i]}">${entries[i]} - ${values[i]}</option>`;
+    select[0].innerHTML += `<option value="${keys[i]}">${keys[i]} - ${values[i]}</option>`;
 
-    select[1].innerHTML += `<option value="${entries[i]}">${entries[i]} - ${values[i]}</option>`;
+    select[1].innerHTML += `<option value="${keys[i]}">${keys[i]} - ${values[i]}</option>`;
   }
 }
 
 getCurrencies();
 
-// async function getData() {
-//   const response = await fetch(url);
-//   const data = await response.json();
-//   console.log(data);
-// }
+async function getData() {
+  let fromOptions = document.querySelector("#from");
+  let toOptions = document.querySelector("#to");
+  let fromValue = fromOptions.options[fromOptions.selectedIndex].value;
+  let fromText = fromOptions.options[fromOptions.selectedIndex].text;
+  let toValue = toOptions.options[toOptions.selectedIndex].value;
+  let toText = toOptions.options[toOptions.selectedIndex].text;
 
-// getData();
+  const url = `https://api.exchangerate.host/convert?from=${fromValue}&to=${toValue}&amount=${input.value}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  resultHeading.innerText = `${input.value} ${fromText.slice(6)}s =
+${data.result} ${toText.slice(6)}s`;
+
+  fromToText.innerText = `1 ${fromValue} = ${data.info.rate} ${toValue}`;
+  toFromText.innerText = `1 ${toValue} = ${1 / data.info.rate} ${fromValue}`;
+}
+
+async function getSwapData() {
+  let fromOptions = document.querySelector("#from");
+  let toOptions = document.querySelector("#to");
+  let fromValue = fromOptions.options[fromOptions.selectedIndex].value;
+  let fromText = fromOptions.options[fromOptions.selectedIndex].text;
+  let toValue = toOptions.options[toOptions.selectedIndex].value;
+  let toText = toOptions.options[toOptions.selectedIndex].text;
+
+  [fromValue, toValue] = [toValue, fromValue];
+  [fromText, toText] = [toText, fromText];
+
+  const url = `https://api.exchangerate.host/convert?from=${fromValue}&to=${toValue}&amount=${input.value}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  resultHeading.innerText = `${input.value} ${fromText.slice(6)}s =
+${data.result} ${toText.slice(6)}s`;
+
+  fromToText.innerText = `1 ${fromValue} = ${data.info.rate} ${toValue}`;
+  toFromText.innerText = `1 ${toValue} = ${1 / data.info.rate} ${fromValue}`;
+}
+
+button.addEventListener("click", getData);
+
+swap.addEventListener("click", getSwapData);
